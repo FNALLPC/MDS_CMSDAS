d2492f450033a021487f0f932bca1d2601911cbb
christina <christiw@fnal.gov>
2025-12-25 16:27:55 -0600

    update

--- scripts/run_combine.py
+++ scripts/run_combine.py
@@ -26,16 +26,13 @@ if __name__ == "__main__":
     #list all the DT cards
     CMSSW_BASE = os.getenv('CMSSW_BASE')
 
-    card_dir= CMSSW_BASE + "/src/solution/MDS_CMSDAS/combine/datacards/"
+    card_dir= CMSSW_BASE + "/src/MDS_CMSDAS/combine/datacards/"
 
     output_tree_dir = card_dir.replace('datacards', 'limitTrees')
-    print(output_tree_dir)
+    print("output tree directory", output_tree_dir)
     if not os.path.exists(output_tree_dir):os.system("mkdir -p %s"%output_tree_dir)
     ctau_list = [5,10,15,20,30,40, 50,60, 100, 125,150,200,300,500,600,700,800,900,1000, 2000,3000,4000, 5000, 6000,7000,8000,10000, 20000,30000,50000, 100000, 200000, 300000, 500000, 1000000, 2000000, 3000000, 5000000, 6000000, 10000000] #mm
 
-
-
-    ##ctau_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     sample_name = 'ggH_HToSSTobbbb_mh125_mx40'
     for ctau in ctau_list:
         ctau_string = str(ctau)
@@ -45,11 +42,9 @@ if __name__ == "__main__":
         if not os.path.isfile(card_dir + name + '.txt'):
     	    print(card_dir + name + '.txt')
     	    continue
-        #normalization = readNorm(twoTag_card_dir + twoTag_name + '.txt')
         norm = readNorm(card_dir + name + '.txt')
 
         if norm == 0.0:continue
         comb_limit = "combine -M AsymptoticLimits  {card_dir}/{card}.txt -n.{card} --setParameters norm={norm} --freezeParameter=norm".format(card_dir=card_dir,card=name,norm = 1./norm)
-        #print(comb_limit)
         os.system(comb_limit)
         os.system("mv higgsCombine.{}.AsymptoticLimits.mH120.root {}/".format(name, output_tree_dir))
